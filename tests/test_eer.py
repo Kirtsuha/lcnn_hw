d8e842f1d27182637289257812cbdd61a65a991f
Kirtsuha <batukhtin.kir@gmail.com>
2026-07-28 22:50:36 +0300

    added balancer

--- tests/test_eer.py
+++ tests/test_eer.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from calculate_eer import compute_eer as official_compute_eer
 from src.metrics.eer import compute_eer
 
 
@@ -15,3 +16,14 @@ def test_eer_is_invariant_to_monotonic_shift():
     first, _ = compute_eer(bonafide, spoof)
     second, _ = compute_eer(bonafide + 10, spoof + 10)
     assert first == second
+
+
+def test_eer_matches_provided_course_implementation():
+    bonafide = np.array([0.2, 0.5, 0.5, 0.9])
+    spoof = np.array([0.1, 0.5, 0.5, 0.8])
+
+    expected_eer, expected_threshold = official_compute_eer(bonafide, spoof)
+    actual_eer, actual_threshold = compute_eer(bonafide, spoof)
+
+    assert actual_eer == expected_eer
+    assert actual_threshold == expected_threshold
